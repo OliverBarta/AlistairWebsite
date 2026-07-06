9f901792f232d89d7954c7553b8d8604c2594484
OliverBarta <barta3738@gmail.com>
2026-07-06 03:16:24 -0400

    pls fix pt2

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <>
-      <Router>
+      <Router basename='/NOTHINGG'>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Cart" element={<Cart />} />
