547b5208cfdb4e44bf0dc7e49460f9e9aeda6c16
OliverBarta <barta3738@gmail.com>
2026-07-06 19:06:10 -0400

    made the bugged items remove button formatted correctly

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -94,18 +94,20 @@ function Cart() {
                 ) : (
                     <div className="cartList">
                         {cartItems.map(item => {
-                            if (!verifyItem(item.id)) {
+                            if (verifyItem(item.id)) {
                                 return (
                                     <div key={item.id} className="listingCart">
+                                        Item bugged
                                         <button
                                             className="removeItemButton"
+                                            style={{ marginLeft: 'auto'}}
                                             onClick={() => removeOneFromCart(item.id)}
                                             aria-label={`Remove one ${item.name} from cart`}
                                         >
                                             Remove
                                         </button>
                                     
-                                    Item bugged</div>
+                                    </div>
                                 );
                             } else {
 
